4d97224e5ca875050ce2fb23668d16748cea62ad
PasiKl <pasi.kleimola@gmail.com>
2022-04-29 13:18:31 +0300

    Fix player movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,108 @@
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float speed, h, v;
+
+    bool flip;
+
+    string state;
+
     Animator anim;
+    Rigidbody2D rb;
+    SpriteRenderer rend;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        speed = 20.0f;
+
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        rend = GetComponent<SpriteRenderer>();
+
+        anim.Play("PlayerIdle");
+
+        state = "idle";
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool move;
+        float x, y;
+
+        move = CheckInput();
+
+        switch(state)
+        {
+            case "idle":
+                // move = CheckInput();
+ 
+                if(move)
+                {
+                    anim.Play("PlayerWalk");
+                
+                    state = "walk";
+                }
+
+                break;
+
+            case "walk":
+                if(!move)
+                {
+                    anim.Play("PlayerIdle");
+                
+                    state = "idle";
+                }
+                // else
+                //     Flip(h);
+
+                break;
+        }
+
+        x = Math.Sign(h) * speed * Time.deltaTime;
+        y = Math.Sign(v) * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + new Vector3(x, y, 0));
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        Debug.Log("hit");
+    }
+
+    bool CheckInput()
+    {
+        h = Input.GetAxis("Horizontal");        
+        v = Input.GetAxis("Vertical");
+
+        if(h != 0)
+        {
+            Flip(h);
+
+            return true;
+        }
         
+        if(v != 0)
+            return true;
+
+        return false;
+    }
+
+    void Flip(float h)
+    {
+        if(h < 0.002f)
+            flip = true;
+        else if(h > 0.002f)
+            flip = false;
+
+        rend.flipX = flip;
     }
 }
+
